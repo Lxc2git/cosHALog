bba6609a0e0e13717f8f9a69c3efcc85c4724816
Lxc2git <1224690685@qq.com>
2025-09-04 13:46:40 +0800

    Update tf-idf.py

--- semantic/tf-idf.py
+++ semantic/tf-idf.py
@@ -11,14 +11,14 @@ import math
 import operator
 
 
-PRETRAINED_MODEL_NAME = "bert-base-chinese"  # 指定繁簡中文 BERT-BASE 預訓練模型
-PRETRAINED_MODEL_NAME1 = "bert-base-cased"  # 指定 BERT-BASE 預訓練模型
+PRETRAINED_MODEL_NAME = "bert-base-chinese"  
+PRETRAINED_MODEL_NAME1 = "bert-base-cased"  
 
 data = pd.read_csv(r'HDFS templates changed.csv')
 template = data.values[:, 1]
 # print(template)
 
-# 取得此預訓練模型所使用的 tokenizer
+# tokenizer
 tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME1)
 # bert = BertModel.from_pretrained(PRETRAINED_MODEL_NAME1)
 
@@ -34,30 +34,29 @@ for i in range(29):
 # print(dataset)
 
 """
-函数说明：特征选择TF-IDF算法
+TF-IDF
 Parameters:
-     list_words:词列表
+     list_words:wordd list
 Returns:
-     dict_feature_select:特征选择词字典
+     dict_feature_select
 """
 
 
 def feature_select(list_words):
-    # 总词频统计
-    doc_frequency = defaultdict(int)  # 全文档各单词出现次数
+    doc_frequency = defaultdict(int)  
     for word_list in list_words:
         for i in word_list:
             doc_frequency[i] += 1
 
-    # 计算每个词的TF值
-    word_tf = {}  # 存储每个词的tf值
+    # compute TF value
+    word_tf = {}  # store tf value
     for i in doc_frequency:
         word_tf[i] = doc_frequency[i] / sum(doc_frequency.values())
 
-    # 计算每个词的IDF值
+    # compute IDF value
     doc_num = len(list_words)
-    word_idf = {}  # 存储每个词的idf值
-    word_doc = defaultdict(int)  # 存储包含该词的文档数
+    word_idf = {}  # store idf value
+    word_doc = defaultdict(int)  
     for i in doc_frequency:
         for j in list_words:
             if i in j:
@@ -65,19 +64,19 @@ def feature_select(list_words):
     for i in doc_frequency:
         word_idf[i] = math.log(doc_num / (word_doc[i]))
 
-    # 计算每个词的TF*IDF的值
+    # compute TF*IDF value
     word_tf_idf = {}
     for i in doc_frequency:
         word_tf_idf[i] = word_tf[i] * word_idf[i]
 
-    # 对字典按值由大到小排序
+    # descending order
     # dict_feature_select = sorted(word_tf_idf.items(), key=operator.itemgetter(1), reverse=True)
     dict_idf_select = sorted(word_idf.items(), key=operator.itemgetter(1), reverse=True)
     return dict_idf_select
 
 
 if __name__ == '__main__':
-    features = feature_select(dataset)  # 所有词的TF-IDF值
+    features = feature_select(dataset)  # all TF-IDF value
     print(features)
     print(len(features))
     df = pd.DataFrame(features)
